70a690353bd2e9a6835a981121a3ced03e154e4f
Hieu Nguyen <hieunguyenel@gmail.com>
2023-01-19 11:09:36 +0700

    add not include insllation option

--- src/pages/api/delivery.ts
+++ src/pages/api/delivery.ts
@@ -13,23 +13,34 @@ export default function handler(
   console.log('start deliverying logic');
   console.log('body', req.body);
   const {items, locale} = req.body.rate;
-  let deliveryCost = 0;
+  let deliveryCostIncludeInstallation = 0;
+  let deliveryCostWithoutInstallation = 0;
   for(const item of items) {
     if (item.sku == 'SF-1105') {
-      deliveryCost += 30000 * item.quantity;
+      deliveryCostIncludeInstallation += 30000 * item.quantity;
+      deliveryCostWithoutInstallation += 6000 * item.quantity;
     }
   }
   console.log('items', items);
   if (locale == 'en-GB') {
     res.status(200).json(
       {
-        rates: {
-          service_name: 'Delivery (Include Installation)', 
-          description: '', 
-          service_code: 'delivery_include_installation', 
-          currency: 'GBP', 
-          'total_price': deliveryCost
-        }
+        rates: [
+          {
+            service_name: 'Delivery (Include Installation)', 
+            description: '', 
+            service_code: 'delivery_include_installation', 
+            currency: 'GBP', 
+            'total_price': deliveryCostIncludeInstallation
+          },
+          {
+            service_name: 'Delivery (Not Include Installation)', 
+            description: '', 
+            service_code: 'delivery_not_include_installation', 
+            currency: 'GBP', 
+            'total_price': deliveryCostWithoutInstallation
+          },
+        ]
       }
     );
   }
